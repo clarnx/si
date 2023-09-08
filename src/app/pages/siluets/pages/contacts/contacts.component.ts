457fc576225a2068d7226aa0d41366953a1dd7d7
Arturs Buls <arturs.buls@gmail.com>
2023-09-08 12:09:06 +0300

    Contacts page WIP

--- src/app/pages/siluets/pages/contacts/contacts.component.ts
+++ src/app/pages/siluets/pages/contacts/contacts.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatIconModule } from '@angular/material/icon';
+import { MatDividerModule } from '@angular/material/divider';
 
 @Component({
   selector: 'app-contacts',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MatIconModule, MatDividerModule],
   templateUrl: './contacts.component.html',
   styleUrls: ['./contacts.component.scss']
 })
